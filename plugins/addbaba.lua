9f9252b681870c543958cc192d9b5d465574e1ed
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-22 04:23:44 -0700

    Update addbaba.lua

--- plugins/addbaba.lua
+++ plugins/addbaba.lua
@@ -7,17 +7,17 @@ end
         if not is_momod or not is_owner then
     return "Only Onwers Can Add iman!"
 end
-    local user = 'user#id'
+    local user = 'user#id192783232'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
     return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
 end
 return {
     usage = {
-      "Addadmin:babamo alan miaram . be babam salam konid. "
+      "Addadmin:Xface alan miaram . be babam salam konid. "
       },
     patterns = {
-        "^#([Aa]ddbaba)$"
+        "^#([Aa]ddx)$"
         },
     run = run
 }
